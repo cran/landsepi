b273cfee7d3777e16737a5ec78b8bbf0d2bcd0a9
Jean-François Rey <jean-francois.rey@inrae.fr>
2023-01-16 17:20:11 +0000

    version 1.2.4

--- R/landsepi.R
+++ R/landsepi.R
@@ -39,7 +39,7 @@
 #' @details \tabular{ll}{
 #'          Package: \tab landsepi\cr
 #'          Type: \tab Package\cr
-#'          Version: \tab 1.2.3\cr
+#'          Version: \tab 1.2.4\cr
 #'          Date: \tab 2022-01-16\cr
 #'          License: \tab GPL (>=2)\cr
 #'          }
